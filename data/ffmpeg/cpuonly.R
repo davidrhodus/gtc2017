80351a5ed99a93bf5caa0e72ca32233d7d907232
Peter Steinbach <steinbac@mpi-cbg.de>
2017-05-10 14:50:21 +0200

    added input size from shape info

--- data/ffmpeg/cpuonly.R
+++ data/ffmpeg/cpuonly.R
@@ -6,36 +6,17 @@ library(ggthemes)
 library(tidyr)
 
 
-## add_colnames_for_enc = function(df){
-##     colnames(df) = c("filename","sizeof_pixel","shape","n_elements","time_mus","final_bytes","id","comment")
-
-##     df = df %>% separate(comment, into = c("pipeline", "threads","timestamp"), sep = "\\|")
-##     df$threads = as.integer(gsub("threads","",df$threads))
-##     df$filename = gsub("\\.tif","",df$filename)
-##     return(df)
-## }
-
-## add_colnames_for_dec = function(ddf){
-##     colnames(ddf) = c("mode","filename","pipeline","realtime_s","user_s","sys_s","exit_code","threads","timestamp")
-##     return(ddf)
-## }
-
-## add_colnames_for_qual = function(qdf){
-##     colnames(qdf) = c("filename","pipeline","timestamp","bits_p_sample","left_drange","mse","nrmse","psnr","right_drange")
-##     return(qdf)
-## }
-
 
 enc_df = read_csv("sqy-corpus-encode.csv") %>% filter(exit==0) %>% select(-flags, -timestamp)
 
-## gr_enc_df = enc <- enc_df %>%
-##     group_by(filename, sizeof_pixel,n_elements,final_bytes,pipeline,threads) %>%
-##     summarize( mn_time_mus = mean(time_mus), md_time_mus = median(time_mus), sd_time_mus = sd(time_mus)) %>%
-##     mutate( size_mb = n_elements*sizeof_pixel/(1024*1024), size_gb = size_mb/(1024),
-##            md_ingest_bw_mb_per_sec = size_mb*10e6/(md_time_mus),
-##            mn_ingest_bw_mb_per_sec = size_mb*10e6/(mn_time_mus),
-##            sd_ingest_bw_mb_per_sec = size_mb*10e6/(sd_time_mus)
-##            )
+info_df = read_csv("sqy-corpus-info.csv") %>% mutate(filename = gsub(".tif","",filename))
+info_df = info_df %>%
+    mutate(parent_filename = filename, bytesof_pixel = bits_per_pixel/8) %>%
+    separate(shape, into = c("zshape", "yshape","xshape"), sep = "x", convert = T, remove = F) %>%
+    mutate(n_elems = zshape*yshape*xshape,
+           size_bytes = n_elems*bytesof_pixel ,
+           yuv420_size_bytes = n_elems*6/4.)
+
 
 #encnv_df = read_csv("sqy-corpus-encode-nvprof.csv") %>% filter(exit==0)
 qual_df = read_csv("sqy-corpus-quality.csv") %>% select(-flags, -timestamp)
@@ -44,8 +25,13 @@ qual_df = read_csv("sqy-corpus-quality.csv") %>% select(-flags, -timestamp)
 combined_enc = inner_join(enc_df,qual_df,by = c("filename"="filename",
                                              "shorthand"="shorthand")) 
 
+glimpse(combined_enc)
+combined_enc = combined_enc %>% mutate( parent_filename = gsub("(_unweighted|weighted_power_3_1)","",filename))
+combined_enc = inner_join(combined_enc,info_df,by = "parent_filename")
+
+
 combined_enc = combined_enc %>%
-    mutate( ingest_bw_mb_per_sec = raw_size_bytes/(1024*1024*realtime_sec),
+    mutate( ingest_bw_mb_per_sec = yuv420_size_bytes/(1024*1024*realtime_sec),
            encoder = gsub("_ultrafast","",shorthand),
            compression_ratio = raw_size_bytes/encoded_size_bytes)
 
